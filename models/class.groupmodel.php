10734699d4e932109abfe9f31f4cc80605750bce
Bogdanova Olga <o_bogdanova@inbox.ru>
2020-11-06 23:03:46 +0300

    Issues-117: don't move any discussions

--- models/class.groupmodel.php
+++ models/class.groupmodel.php
@@ -1020,6 +1020,8 @@ class GroupModel extends Gdn_Model {
      *
      */
     public function canMoveDiscussion($discussion) {
+        // Don't move any discussions
+        /*
         $groupID = $discussion->GroupID;
         if(!$groupID ) {
             return $this->canEditDiscussion($discussion);
@@ -1031,7 +1033,7 @@ class GroupModel extends Gdn_Model {
         if($groupRole === GroupModel::ROLE_LEADER || Gdn::session()->UserID === $group->OwnerID
             || Gdn::session()->checkPermission(GroupsPlugin::GROUPS_MODERATION_MANAGE_PERMISSION)) {
             return true;
-        }
+        } */
         return false;
     }
 
