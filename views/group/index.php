04377232804adb80fa04730868d4a1778529341c
Bogdanova Olga <o_bogdanova@inbox.ru>
2022-01-21 22:09:02 +0300

    Support self-service flag

--- views/group/index.php
+++ views/group/index.php
@@ -26,30 +26,33 @@ $ViewLocation = $this->fetchViewLocation('discussions', 'discussions', 'vanilla'
 <?php echo writeGroupHeader($Group, true, $Owner);?>
 
 <div class="">
-    <div class="Group-Box Group-Announcements Section-DiscussionList">
-        <h1 class="H">Announcements</h1>
-        <div class="PageControls">
-            <div class="Button-Controls">
-                <?php
+    <?php
+    if(!isSelfService() || (isSelfService() && is_object($Announcements) && count($Announcements->result()) > 0)) { ?>
+        <div class="Group-Box Group-Announcements Section-DiscussionList">
+            <h1 class="H">Announcements</h1>
+            <div class="PageControls">
+                <div class="Button-Controls">
+                    <?php
 
-                if($groupModel->canAddNewAnnouncement($Group) && !hideInMFE()) {
-                   echo anchor('New Announcement', $this->data('DefaultAnnouncementUrl'), 'Button Primary', '');
-                }
-                ?>
+                    if($groupModel->canAddNewAnnouncement($Group) && !hideInMFE()) {
+                        echo anchor('New Announcement', $this->data('DefaultAnnouncementUrl'), 'Button Primary', '');
+                    }
+                    ?>
+                </div>
             </div>
+            <?php
+            if (is_object($Announcements) && count($Announcements->result()) > 0) {
+                echo '<ul class="DataList Discussions">';
+                foreach ($Announcements->result() as $Discussion) {
+                    writeDiscussion($Discussion, $this, $Session);
+                }
+                echo '</ul>';
+            } else {
+                ?>
+                <div class="EmptyMessage">No announcements were found.</div>
+            <?php } ?>
         </div>
-       <?php
-        if (is_object($Announcements) && count($Announcements->result()) > 0) {
-            echo '<ul class="DataList Discussions">';
-            foreach ($Announcements->result() as $Discussion) {
-                writeDiscussion($Discussion, $this, $Session);
-            }
-            echo '</ul>';
-          } else {
-        ?>
-        <div class="EmptyMessage">No announcements were found.</div>
-        <?php } ?>
-    </div>
+    <?php } ?>
     <?php
        // echo '<div class="Group-Info ClearFix clearfix"><div class="Group-Box Group-MembersPreview"> <div class="PageControls">'.
        // '<h1 class="Groups H">Members</h1></div>';
